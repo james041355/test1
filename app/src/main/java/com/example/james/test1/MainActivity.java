a9e2bdba53bb0322a214a789a00b8a5230c054d7
james <james041355@yahoo.com.tw>
2020-01-31 20:29:36 +0800

    v1.1

--- app/src/main/java/com/example/james/test1/MainActivity.java
+++ app/src/main/java/com/example/james/test1/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.james.test1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.newa);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        
     }
+
 }
