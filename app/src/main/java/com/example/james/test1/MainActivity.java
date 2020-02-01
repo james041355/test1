b43e0971b9491d8a59a97223755af988f7ba9020
james <james041355@yahoo.com.tw>
2020-02-01 11:04:59 +0800

    v1.2

--- app/src/main/java/com/example/james/test1/MainActivity.java
+++ app/src/main/java/com/example/james/test1/MainActivity.java
@@ -4,22 +4,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.newa);
+        setid();
+        onclick();
+       // Integer.parseInt("15")
+
+    }
+
+    void onclick() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+            textView.setText("hello");
             }
         });
-
-        
     }
 
+    void setid() {
+        button = findViewById(R.id.newa);
+        textView=findViewById(R.id.show1);
+    }
+
+    void toast(String message)
+    {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
 }
